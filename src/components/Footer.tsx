127004404c30d3a5af30a53499c664b607df1952
YordanYordanov90 <157980191+YordanYordanov90@users.noreply.github.com>
2024-05-16 20:45:19 +0300

    last

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import React from "react";
-import Image from "next/image";
+import Image from 'next/image';
 
 type MainFooterProps = React.DetailedHTMLProps<
   React.HTMLAttributes<HTMLElement>,
@@ -21,8 +21,8 @@ const sections = [
   {
     name: "Products",
     links: [
-      { name: "Sales software", href: "#", target: "_self" },
-      { name: "Features", href: "#", target: "_self" },
+      { name: "Sales sofwate", href: "#", target: "_self" },
+      { name: "Featurs", href: "#", target: "_self" },
       { name: "Privacy", href: "#", target: "_self" },
       { name: "Marketplace", href: "#", target: "_self" },
       { name: "Status", href: "#", target: "_self" },
@@ -32,8 +32,8 @@ const sections = [
   {
     name: "Discover",
     links: [
-      { name: "Partner Program", href: "#", target: "_self" },
-      { name: "Get our newsletter", href: "#", target: "_self" },
+      { name: "Parner Program", href: "#", target: "_self" },
+      { name: "Get out newslette", href: "#", target: "_self" },
       { name: "Sales Pipeline", href: "#", target: "_self" },
       { name: "What is CRM?", href: "#", target: "_self" },
       { name: "CRM Components", href: "#", target: "_self" },
@@ -53,18 +53,18 @@ const sections = [
 
 const section2 = {
   links: [
-    { name: "Terms of service", href: "#", target: "_self" },
-    { name: "Privacy Policy", href: "#", target: "_self" },
+    { name: "Tearms of service", href: "#", target: "_self" },
+    { name: "Privacy Police", href: "#", target: "_self" },
     { name: "Cookies", href: "#", target: "_self" },
   ],
 };
 
 export default function MainFooter(props: MainFooterProps) {
   return (
-    <footer {...props} className="border-t py-2 pt-28 ">
-      <div className="container mx-auto flex flex-col md:flex-row justify-between">
-        <section className="mb-8 md:mb-0 md:w-1/3 ml-24">
-          <Link className="text-[25px] font-black leading-[28px]" href="/">
+    <footer {...props} className="border-t py-2 pt-28">
+      <div className="container max-w-[1140px] mx-auto flex md:flex-row flex-col justify-between">
+        <section>
+          <Link className="text-[25px] font-black leading-[28px]" href={"/"}>
             Rooby
           </Link>
           <p className="line-clamp-3 sm:line-clamp-2 md:line-clamp-4 pt-7">
@@ -72,23 +72,32 @@ export default function MainFooter(props: MainFooterProps) {
             <br /> Our team created a fully integrated <br /> sales and
             marketing solution for SMBs
           </p>
-          <div className="mt-[22px] flex items-center gap-4 justify-center md:justify-start">
+          <div className="mt-[22px] flex items-center gap-4 max-sm:justify-center">
+            <div>
             <Image
-              src="/SocialMedia.png"
-              alt="Social Media"
-              width={100}
-              height={100}
-            />
+        src="/SocialMedia.png"
+        alt="Social Media"
+        width={100} 
+        height={100} 
+      />
+            </div>
+          </div>
+          <div>
+            <a href=""></a>
           </div>
         </section>
-        <nav className="grid grid-cols-2 gap-1 md:grid-cols-4 w-full md:w-2/3">
-          {sections.map((section) => (
+        <nav className="grid xs:grid-cols-2  gap-4 md:gap-0 md:flex space-x-5 justify-evenly w-full max-w-[768px]">
+          {sections?.map((section) => (
             <section key={section.name} className="flex flex-col">
-              <h2 className="headline-h5 font-medium mb-2">{section.name}</h2>
+              <h2 className="headline-h5 font-medium mb-2">{section?.name}</h2>
               <ul className="space-y-[10px]">
-                {section.links.map((link, i) => (
+                {section?.links?.map((link, i) => (
                   <li key={i}>
-                    <Link className="body" href={link.href} target={link.target}>
+                    <Link
+                      className="body"
+                      href={link.href}
+                      target={link.target}
+                    >
                       {link.name}
                     </Link>
                   </li>
@@ -98,9 +107,9 @@ export default function MainFooter(props: MainFooterProps) {
           ))}
         </nav>
       </div>
-      <div className="container border-t flex flex-col md:flex-row items-center justify-between mx-auto py-10 mt-[50px] md:mt-[134px] max-w-[1140px]">
-        <p className="mb-4 md:mb-0">© Copyright 2024</p>
-        <ul className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-8 justify-center mb-4 md:mb-0">
+      <div className="container border-t flex items-center justify-between m-auto py-10 mt-[134px] max-w-[1140px]">
+        <p className="">© Copyright 2024</p>
+        <ul className="flex flex-row space-x-8 justify-center ml-20">
           {section2.links.map((link, index) => (
             <li className="font-bold" key={index}>
               <Link href={link.href} target={link.target}>
@@ -111,11 +120,11 @@ export default function MainFooter(props: MainFooterProps) {
         </ul>
         <select
           className="text-black border-transparent rounded-lg px-2"
-          name="language"
-          id="language-select"
+          name="English"
+          id=""
         >
-          <option value="English">English</option>
-          <option value="Български">Български</option>
+          <option value="English"> English</option>
+          <option value="">Български</option>
         </select>
       </div>
     </footer>
