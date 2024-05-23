ff9cbad4380bc843e760c7188b0631eb08704c18
YordanYordanov90 <157980191+YordanYordanov90@users.noreply.github.com>
2024-05-23 22:10:44 +0300

    last

--- src/app/contacts/ContactsForm.tsx
+++ src/app/contacts/ContactsForm.tsx
@@ -1,4 +1,4 @@
-
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
 
 const ContactsForm = () => {
   const containerStyle = {
@@ -24,6 +24,15 @@ const ContactsForm = () => {
               <p>Plovdiv, Bulgaria, bul Vasil Levski 96</p>
             </div>
             <div>
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2957.4245432803646!2d24.74086947606983!3d42.162599647553655!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14acd18de452a06b%3A0x838e93baa43989e5!2sSeveren%2C%20Vasil%20Levski%20St%2096%2C%204003%20Plovdiv!5e0!3m2!1sen!2sbg!4v1716491079609!5m2!1sen!2sbg"
+                width="458"
+                height="288"
+                style={{border:'0'}}
+                
+                loading="lazy"
+
+              ></iframe>
             </div>
           </div>
           <div className="bg-white p-8 rounded-lg shadow-md">
